c13e131145ec6bcf3e9d3b7ccf59c060980349c3
Adam Snodgrass <overra@gmail.com>
2016-10-01 19:45:10 -0500

    Fix issue that caused the same video to popout after changing switching videos

--- src/watch.js
+++ src/watch.js
@@ -4,12 +4,18 @@ onReady(initWatchScript)
 
 function embedUrl (videoId, currentTime) {
   return 'https://www.youtube.com/embed/' + videoId + '?autoplay=1' +
-    '&start=' + Math.round(currentTime)  
+    '&start=' + Math.round(currentTime)
+}
+
+function getMetadata() {
+  const videoElement = document.querySelector('video')
+  const videoId = location.href.split('v=')[1].split('&')[0]
+
+  return [videoId, videoElement]
 }
 
 function initWatchScript() {
-  const videoElement = document.querySelector('video')
-  const videoId = location.href.split('v=')[1].split('&')[0]
+  const [videoId, videoElement] = getMetadata()
 
   chrome.runtime.sendMessage({
     action: 'popout',
@@ -20,6 +26,7 @@ function initWatchScript() {
     if (!msg) return
 
     if (msg.action === 'popout') {
+      const [videoId, videoElement] = getMetadata()
       chrome.runtime.sendMessage({
         action: 'popout',
         url: embedUrl(videoId, videoElement.currentTime)
